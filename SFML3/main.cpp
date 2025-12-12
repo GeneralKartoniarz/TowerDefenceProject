97b8ec300985f610a3f14347a274c36be13028a6
Jan Pałka <90349664+GeneralKartoniarz@users.noreply.github.com>
2025-12-12 20:03:43 +0100

    Dodaj pliki projektów.

---
+++ SFML3/main.cpp
@@ -0,0 +1,17 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "TEST");
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+}
